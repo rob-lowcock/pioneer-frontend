41e31f6ce03b3ff88cb1a7d402858db920ee14ac
Rob Lowcock <rob.lowcock@gmail.com>
2021-12-30 22:43:51 +0000

    Add minor styling updates to archive buttons

--- src/components/ArchiveButton.vue
+++ src/components/ArchiveButton.vue
@@ -25,10 +25,10 @@ export default {
 
 <template>
   <div class="flex flex-row text-center text-white">
-    <button class="bg-blue basis-full block w-100 px-4 py-2" @click="hideConfirm = !hideConfirm">Clear Board</button>
+    <button class="bg-blue basis-full block w-100 px-8 py-2 hover:bg-navy" @click="hideConfirm = !hideConfirm">Clear Board</button>
   </div>
   <div class="flex flex-row text-center text-white" v-bind:class="{'hidden':hideConfirm}">
-    <button class="basis-1/2 bg-green" @click="archive">Yes</button>
-    <button class="basis-1/2 bg-red" @click="hideConfirm = true">Cancel</button>
+    <button class="basis-1/2 bg-green hover:bg-emerald-900 px-2" @click="archive">Confirm</button>
+    <button class="basis-1/2 bg-red hover:bg-rose-900 px-2" @click="hideConfirm = true">Cancel</button>
   </div>
 </template>
--- src/components/RetroColumn.vue
+++ src/components/RetroColumn.vue
@@ -62,7 +62,7 @@ export default {
         <h1 class="py-2 my-2 text-l font-bold text-gray-500 border-b-2 border-gray-500"><img :src="icon" alt="" class="float-left pr-2">{{heading}}</h1>
         <form @submit="submit($event)" class="bg-white shadow-md my-2 p-4">
             <div class="flex">
-            <input type="text" name="title" :placeholder="hint" class="flex-1 focus:outline-none focus-visible:outline-none">
+            <input type="text" name="title" :placeholder="hint" class="flex-1 focus:outline-none focus-visible:outline-none caret-blue">
             <button type="submit" class="ml-4 flex-none p-2 bg-blue text-white justify-self-end hover:bg-navy">
                 <svg width="16" height="16" viewBox="0 0 16 16" fill="none" xmlns="http://www.w3.org/2000/svg">
                 <g clip-path="url(#clip0_10_24)">
